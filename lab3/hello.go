d585c8c2d269944c6af81cfadd5a36ba43b9afea
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-11-17 00:25:30 +0700

    Update lab3

--- lab3/hello.go
+++ lab3/hello.go
@@ -1,41 +1,45 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Printf("Receive request path: %s\n", r.URL.Path)
-  if r.URL.Path == "/" {
-    files, err := ioutil.ReadDir("/tmp/resources/")
-    if err != nil {
-      http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-      return
-    }
-    _, _ = fmt.Fprintf(w, "<h1>Hi, there!</h1><br>")
-    for _, file := range files {
-      fileName := file.Name()
-      link := fmt.Sprintf("<a href='/%s'>%s</a><br>", fileName, fileName)
-      _, _ = fmt.Fprintf(w, link)
-    }
-  } else {
-    filePath := "/tmp/resources/" + r.URL.Path[1:] + ".html"
-    if _, err := os.Stat(filePath); err == nil {
-      http.ServeFile(w, r, filePath)
-    } else {
-      http.Redirect(w, r, "/error", http.StatusFound)
-    }
-  }
+	fmt.Printf("Receive request path: %s\n", r.URL.Path)
+	if r.URL.Path == "/" {
+		files, err := os.ReadDir("/tmp/resources/")
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		_, _ = fmt.Fprintf(w, "<h1>Hi, there!</h1><br>")
+		for _, file := range files {
+			fileName := file.Name()
+			link := fmt.Sprintf("<a href='/%s'>%s</a><br>", fileName, fileName)
+			_, _ = fmt.Fprintf(w, link)
+		}
+	} else {
+		filePath := "/tmp/resources/" + r.URL.Path[1:]
+		if filepath.Ext(filePath) != ".html" {
+			filePath += ".html"
+		}
+		if _, err := os.Stat(filePath); err == nil {
+			http.ServeFile(w, r, filePath)
+		} else {
+			http.Redirect(w, r, "/error", http.StatusFound)
+		}
+	}
 }
 
 func main() {
-  fmt.Printf("Server started")
-  http.HandleFunc("/", helloWorldHandler)
-  http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
-    _, _ = fmt.Fprintf(w, "<h1>Error: File not found</h1>")
-  })
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Server started")
+	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "<h1>Error: File not found</h1>")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
