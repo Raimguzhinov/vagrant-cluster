5b9e1a3644644a766dbaa60d6efb9d4643a7b43b
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-12-19 16:27:51 +0700

    Add run mpi-test target

--- rgr/job.sh
+++ rgr/job.sh
@@ -1,8 +1,9 @@
-#!/bin/bash 
-#SBATCH -J mpi-hello             
-#SBATCH -n 2                     
-#SBATCH -t 0:10:00               
-#SBATCH -o ob.out         
- 
-mpicc hello.c -o hello 
-mpirun --allow-run-as-root -np 2 ./hello 
+#!/bin/bash
+#SBATCH -J mpi-hello
+#SBATCH -n 2
+#SBATCH -t 0:10:00
+#SBATCH --output=/home/vagrant/web_storage/mpi-hello.out
+#SBATCH --error=/home/vagrant/web_storage/mpi-hello.log
+
+mpicc hello.c -o /home/vagrant/nfs_test/hello
+srun /home/vagrant/web_storage/hello
